ff919f228a18026c714494f3e5dd22ddf1878341
Bernhard Schussek <bernhard.schussek@symfony-project.com>
2011-01-27 15:40:31 +0100

    Fixed autoload.php for use with new ClassLoader component

--- src/autoload.php
+++ src/autoload.php
@@ -2,9 +2,9 @@
 
 $vendorDir = __DIR__.'/vendor';
 
-require_once $vendorDir.'/symfony/src/Symfony/Component/HttpFoundation/UniversalClassLoader.php';
+require_once $vendorDir.'/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
-use Symfony\Component\HttpFoundation\UniversalClassLoader;
+use Symfony\Component\ClassLoader\UniversalClassLoader;
 
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
